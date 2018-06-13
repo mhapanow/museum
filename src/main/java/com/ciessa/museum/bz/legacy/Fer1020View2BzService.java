c92f5c9b85f3ba8061a2fa0748057a07d2f81ea6
Juan Infante <tritonbox@hotmail.com>
2018-06-12 21:12:11 -0500

    Fer1020View2 Segunda entrega

--- src/main/java/com/ciessa/museum/bz/legacy/Fer1020View2BzService.java
+++ src/main/java/com/ciessa/museum/bz/legacy/Fer1020View2BzService.java
@@ -1,8 +1,11 @@
 package com.ciessa.museum.bz.legacy;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,10 +14,12 @@ import org.restlet.resource.Get;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ciessa.museum.bz.RestBaseServerResource;
+import com.ciessa.museum.bz.legacy.TAR0077ViewBzService.TAR0077Adapter;
 import com.ciessa.museum.dao.DataSetDAO;
 import com.ciessa.museum.dao.legacy.AltnamDAO;
 import com.ciessa.museum.dao.legacy.SaldomDAO;
 import com.ciessa.museum.dao.legacy.Tap002DAO;
+import com.ciessa.museum.dao.legacy.Tap014DAO;
 import com.ciessa.museum.exception.ASException;
 import com.ciessa.museum.exception.ASExceptionHelper;
 import com.ciessa.museum.model.DataSet;
@@ -22,6 +27,7 @@ import com.ciessa.museum.model.User;
 import com.ciessa.museum.model.legacy.Altnam;
 import com.ciessa.museum.model.legacy.Saldom;
 import com.ciessa.museum.model.legacy.Tap002;
+import com.ciessa.museum.model.legacy.Tap014;
 
 public class Fer1020View2BzService extends RestBaseServerResource{
 
@@ -38,6 +44,10 @@ public class Fer1020View2BzService extends RestBaseServerResource{
 	
 	@Autowired
 	SaldomDAO myDaoSaldom;
+	
+	@Autowired
+	Tap014DAO myDaoTap014;
+	
 	
 	private SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 	
@@ -60,9 +70,12 @@ public class Fer1020View2BzService extends RestBaseServerResource{
 			String wtod = obtainStringValue("wtod", null);
 			String wvigd = obtainStringValue("wvigd", null);
 			String wvigh = obtainStringValue("wvigh", null);
-			String wimp = obtainStringValue("wimp", null);
+			double wimp = obtainDoubleValue("wimp",0d);
 			
-			boolean rpta = SubRut001(ds, wcta, wbas, wvigd, wtod);
+			Date fecjud = null;
+			Date fecjuh = null;
+			
+			boolean rpta = SubRut001(ds, wcta, wbas, wvigd, wvigh, wtod, fecjud, fecjuh);
 			if (rpta == false)
 			{
 				log.log(Level.SEVERE, "Prueba Error", new Exception());
@@ -71,7 +84,22 @@ public class Fer1020View2BzService extends RestBaseServerResource{
 			}
 			else
 			{
-				SubRut007(ds);
+				ArrayList<Tap014> lstTap014 = SubRut007(ds, wcta, wbas, waca, wpto, wtod, fecjud, fecjuh, wimp);
+
+				// Get the output fields
+				String[] fields = new String[] {
+						"pkid",
+						"member",
+						"DMBK",
+						"DMTYP",
+						"DMACCT",
+						"DMTODF",
+						"DODATE",
+						"DOTIE"
+				};
+				
+				// Obtains the user JSON representation
+				returnValue = getJSONRepresentationFromObject(lstTap014, fields);
 			}
 			
 		} catch (ASException e) {
@@ -91,10 +119,8 @@ public class Fer1020View2BzService extends RestBaseServerResource{
 		return returnValue.toString();
 	}
 	
-	private boolean SubRut001(DataSet ds, String wcta, String wbas, String wvigd, String wtod) {
+	private boolean SubRut001(DataSet ds, String wcta, String wbas, String wvigd, String wvigh, String wtod, Date fecjud, Date fecjuh) throws ParseException {
 		String pgreco;
-		Date fecjud;
-		Date fecjuh;
 		try {
 			if (Integer.parseInt(wcta) > 0) {
 				Tap002 obj = myDaoTap002.getUsingWcta(ds, wcta);
@@ -117,10 +143,6 @@ public class Fer1020View2BzService extends RestBaseServerResource{
 				if (wtod == "X")
 					return false;
 				else {
-					//TODO: Revisar ???
-					//pgreco = wvigd;
-					//julia = SubRut002(ds, pgreco);
-					//fecjud = julia;
 					fecjud = format.parse(wvigd);
 				}
 			}
@@ -139,39 +161,45 @@ public class Fer1020View2BzService extends RestBaseServerResource{
 		return true;
 	}
 	
-	private void SubRut007(DataSet ds, String wcta) {
+	private ArrayList<Tap014> SubRut007(DataSet ds, String wcta, String wbas, String waca, String wpto, String wtod, Date fecjud, Date fecjuh, double wimp) throws ParseException, ASException {
 		Long wdias;
 		Date dexaca;
-		try {
-			Tap002 objTap002 = myDaoTap002.getUsingWcta(ds, wcta);
-			Altnam objAltNam = myDaoAltnam.getUsingWcta(ds, wcta);
-			if (objTap002.getDmcbal().intValue() < 0) {
-				Saldom objSaldom = myDaoSaldom.getUsingWcta(ds, wcta);
-				if (objSaldom != null) {
-					if (objSaldom.getDexaca() > 0) {
-						//TODO: Resolver
-						dexaca = format.parse(objSaldom.getDexaca().toString());
-						long currentTime = Calendar.getInstance().getTime().getTime(); 
-						wdias = Calendar.getInstance().getTime().getTime() - dexaca.getTime();
-					}
-					else
-					{
-						wdias = (long)0;
-					}
+		Tap002 objTap002 = myDaoTap002.getUsingWcta(ds, wcta);
+		Altnam objAltNam = myDaoAltnam.getUsingWcta(ds, wcta);
+		if (objTap002.getDmcbal().intValue() < 0) {
+			Saldom objSaldom = myDaoSaldom.getUsingWcta(ds, wcta);
+			if (objSaldom != null) {
+				if (objSaldom.getDexaca() > 0) {
+					//TODO: Resolver
+					dexaca = format.parse(objSaldom.getDexaca().toString());
+					long currentTime = Calendar.getInstance().getTime().getTime(); 
+					wdias = Calendar.getInstance().getTime().getTime() - dexaca.getTime();
 				}
 				else
+				{
 					wdias = (long)0;
+				}
 			}
 			else
 				wdias = (long)0;
-			//
-			
-		}catch (ASException e) {
-			return;
 		}
+		else
+			wdias = (long)0;
+		//
+		ArrayList<Tap014> lstTap014 = SubRut006(ds, wcta, wbas, waca, wpto, wtod, fecjud, fecjuh, wimp);
+		return lstTap014;
 	}
 	
-	private void SubRut006(DataSet ds, String wcta) {
-		
+	private ArrayList<Tap014> SubRut006(DataSet ds, String wcta, String wbas, String waca, String wpto, String wtod, Date fecjud, Date fecjuh, double wimp) {
+		ArrayList<Tap014> lstTap014 = null;
+		try {
+			if (wcta != "")
+				lstTap014 = myDaoTap014.getUsingWcta(ds, wcta, waca, wpto, wtod, fecjud, fecjuh, wimp);
+			if (wbas != "")
+				lstTap014 = myDaoTap014.getUsingWbas(ds, wbas, waca, wpto, wtod, fecjud, fecjuh, wimp);
+			return lstTap014;
+		}catch (ASException e) {
+			return null;
+		}
 	}
 }
--- src/main/java/com/ciessa/museum/dao/legacy/Tap014DAO.java
+++ src/main/java/com/ciessa/museum/dao/legacy/Tap014DAO.java
@@ -1,13 +1,143 @@
 package com.ciessa.museum.dao.legacy;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 import com.ciessa.museum.exception.ASException;
+import com.ciessa.museum.exception.ASExceptionHelper;
 import com.ciessa.museum.model.DataSet;
-import com.ciessa.museum.model.legacy.Saldom;
+import com.ciessa.museum.model.legacy.Tap014;
+import com.ciessa.museum.tools.HibernateUtil;
 
 public class Tap014DAO {
 
-	public Saldom getUsingWcta(DataSet ds, String wcta) throws ASException {
+	public ArrayList<Tap014> getUsingWcta(DataSet ds, String wcta, String waca, String wpto, String wtod, Date fecjud, Date fecjuh, double wimp) throws ASException {
+		SessionFactory factory = HibernateUtil.getInstance().getSessionFactory();
+		try {
+			Configuration config = new Configuration();
+			config.configure("hibernate_legacy.cfg.xml");
+			config.setProperty("hibernate.connection.url", ds.getUrl());
+			config.setProperty("hibernate.connection.username", ds.getUsername());
+			config.setProperty("hibernate.connection.password", ds.getPassword());
+			factory = config.buildSessionFactory();
+		} catch (Throwable ex) {
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
 		
+		Session session = factory.openSession();
+		Transaction tx = null;
+		Calendar cal;
+
+		try {
+			tx = session.beginTransaction();
+			String queryStr = "FROM TAP014 WHERE DMBK = 1 AND DMTYP = 6 AND DMACCT = :wcta";
+			if (waca == "X")
+				queryStr = queryStr + " AND DMTODF = 1 ";
+			if (wpto == "X")
+				queryStr = queryStr + " AND DMTODF = 2 ";
+			if (wtod == "X")
+				queryStr = queryStr + " AND DMTODF = 9 ";
+			if (null != fecjud && null != fecjuh) {
+				queryStr = queryStr + " AND (";
+				cal = Calendar.getInstance();
+				cal.setTime(fecjud);
+				queryStr = queryStr + "DOAMNX >= " + cal.get(Calendar.YEAR) + new DecimalFormat("000").format(cal.get(Calendar.DAY_OF_YEAR)) +  " AND ";
+				cal.setTime(fecjuh);
+				queryStr = queryStr + "DOAMNX <= " + cal.get(Calendar.YEAR) + new DecimalFormat("000").format(cal.get(Calendar.DAY_OF_YEAR));
+				queryStr = queryStr +  " ) ";
+			}
+			if (wimp > 0)
+				queryStr = queryStr + " AND DOLIMA >= " + wimp;
+			Query q = session.createQuery(queryStr);
+			q.setParameter("wcta", wcta);
+			ArrayList<Tap014> list = (ArrayList<Tap014>)q.list();
+			
+			if( list.size() == 0 ) {
+				tx.rollback();
+				throw ASExceptionHelper.notFoundException(wcta);
+			}
+			
+			session.evict(list);
+			tx.commit();
+			
+			return list;
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			throw ASExceptionHelper.defaultException(e.getMessage(), e);
+		} finally {
+			session.close();
+		}
+	}
+	
+	public ArrayList<Tap014> getUsingWbas(DataSet ds, String wbas, String waca, String wpto, String wtod, Date fecjud, Date fecjuh, double wimp) throws ASException {
+		SessionFactory factory = HibernateUtil.getInstance().getSessionFactory();
+		try {
+			Configuration config = new Configuration();
+			config.configure("hibernate_legacy.cfg.xml");
+			config.setProperty("hibernate.connection.url", ds.getUrl());
+			config.setProperty("hibernate.connection.username", ds.getUsername());
+			config.setProperty("hibernate.connection.password", ds.getPassword());
+			factory = config.buildSessionFactory();
+		} catch (Throwable ex) {
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+		
+		Session session = factory.openSession();
+		Transaction tx = null;
+		Calendar cal;
+
+		try {
+			tx = session.beginTransaction();
+			String queryStr = "FROM TAP014 WHERE DMBK = 1 AND DMTYP = 6 AND DMACCT = :wcta";
+			if (waca == "X")
+				queryStr = queryStr + " AND DMTODF = 1 ";
+			if (wpto == "X")
+				queryStr = queryStr + " AND DMTODF = 2 ";
+			if (wtod == "X")
+				queryStr = queryStr + " AND DMTODF = 9 ";
+			if (null != fecjud && null != fecjuh) {
+				queryStr = queryStr + " AND (";
+				cal = Calendar.getInstance();
+				cal.setTime(fecjud);
+				queryStr = queryStr + "DOAMNX >= " + cal.get(Calendar.YEAR) + new DecimalFormat("000").format(cal.get(Calendar.DAY_OF_YEAR)) +  " AND ";
+				cal.setTime(fecjuh);
+				queryStr = queryStr + "DOAMNX <= " + cal.get(Calendar.YEAR) + new DecimalFormat("000").format(cal.get(Calendar.DAY_OF_YEAR));
+				queryStr = queryStr +  " ) ";
+			}
+			if (wimp > 0)
+				queryStr = queryStr + " AND DOLIMA >= " + wimp;
+			Query q = session.createQuery(queryStr);
+			q.setParameter("wbas", wbas.substring(2-1, 7-1));
+			ArrayList<Tap014> list = (ArrayList<Tap014>)q.list();
+			
+			if( list.size() == 0 ) {
+				tx.rollback();
+				throw ASExceptionHelper.notFoundException(wbas);
+			}
+			
+			session.evict(list);
+			tx.commit();
+			
+			return list;
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			throw ASExceptionHelper.defaultException(e.getMessage(), e);
+		} finally {
+			session.close();
+		}
 	}
 	
 }
