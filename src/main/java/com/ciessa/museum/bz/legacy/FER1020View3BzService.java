804d72107b507f8d37eb22e9c4714d7687d8b0b3
Juan Infante <tritonbox@hotmail.com>
2018-06-14 07:02:59 -0500

    Se crea Fer1020 - View3

--- src/main/java/com/ciessa/museum/bz/legacy/FER1020View3BzService.java
+++ src/main/java/com/ciessa/museum/bz/legacy/FER1020View3BzService.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,9 +28,9 @@ import com.ciessa.museum.model.legacy.Saldom;
 import com.ciessa.museum.model.legacy.Tap002;
 import com.ciessa.museum.model.legacy.Tap014;
 
-public class Fer1020View2BzService extends RestBaseServerResource{
+public class FER1020View3BzService extends RestBaseServerResource {
 
-	public static final Logger log = Logger.getLogger(TAR0077ListBzService.class.getName());
+	public static final Logger log = Logger.getLogger(FER1020View3BzService.class.getName());
 	
 	@Autowired
 	DataSetDAO dsDao;
@@ -47,12 +46,12 @@ public class Fer1020View2BzService extends RestBaseServerResource{
 	
 	@Autowired
 	Tap014DAO myDaoTap014;
-
+	
 	Tap002 tap002 = new Tap002();
 	Altnam altnam = new Altnam();
 	Saldom saldom = new Saldom();
 	Long wdias = new Long(0);
-
+	
 	// Get the output fields
 	String[] fields = new String[] {
 			"WCTA1",
@@ -66,11 +65,12 @@ public class Fer1020View2BzService extends RestBaseServerResource{
 
 	// Get the output fields
 	String[] arrayFields = new String[] {
+			"WCTA1",
 			"DESTIP",
 			"DOLIDO",
+			"DMCMCN",
 			"WSDATC",
 			"WSAMND",
-			"DOAGGI",
 			"WSINTE"
 	};
 		
@@ -94,23 +94,23 @@ public class Fer1020View2BzService extends RestBaseServerResource{
 			String wvigh = obtainStringValue("wvigh", null);
 			double wimp = obtainDoubleValue("wimp",0d);
 			
+			
 			Date fecjud = null;
 			Date fecjuh = null;
 			
-			boolean rpta = SubRut001(ds, wcta, wbas, wvigd, wvigh, wtod, fecjud, fecjuh);
-			if (rpta == false)
+			String rpta = SubRut001(ds, wcta, wbas, wvigd, wvigh, wtod, fecjud, fecjuh);
+			if (!rpta.equals(""))
 			{
-				log.log(Level.SEVERE, "Prueba Error", new Exception());
-				//TODO: Personalizar los mensajes en SubRut001
-				return getJSONRepresentationFromException(ASExceptionHelper.defaultException("Prueba Error", new Exception())).toString();
+				log.log(Level.SEVERE, rpta, new Exception());
+				return getJSONRepresentationFromException(ASExceptionHelper.defaultException(rpta, new Exception())).toString();
 			}
 			else
 			{
-				ArrayList<Tap014> lstTap014 = SubRut007(ds, wcta, wbas, waca, wpto, wtod, fecjud, fecjuh, wimp);
+				ArrayList<Tap014> lstTap014 = SubRut006(ds, wcta, wbas, waca, wpto, wtod, fecjud, fecjuh, wimp);
 
 				// Obtains the user JSON representation
-				Fer1020Adapter adapter = new Fer1020Adapter(tap002, altnam, saldom, wdias);
-				ArrayList<Fer1020SFLAdapter> sflAdapter = new ArrayList<Fer1020View2BzService.Fer1020SFLAdapter>();
+				Fer1020Adapter adapter = new Fer1020Adapter(wbas);
+				ArrayList<Fer1020SFLAdapter> sflAdapter = new ArrayList<FER1020View3BzService.Fer1020SFLAdapter>();
 				for( Tap014 obj : lstTap014) {
 					sflAdapter.add(new Fer1020SFLAdapter(obj));
 				}
@@ -136,35 +136,37 @@ public class Fer1020View2BzService extends RestBaseServerResource{
 		return returnValue.toString();
 	}
 	
-	private boolean SubRut001(DataSet ds, String wcta, String wbas, String wvigd, String wvigh, String wtod, Date fecjud, Date fecjuh) throws ParseException {
+	private String SubRut001(DataSet ds, String wcta, String wbas, String wvigd, String wvigh, String wtod, Date fecjud, Date fecjuh) throws ParseException {
 		try {
+			if (wcta != null && wbas != null) {				
+				if ((Integer.parseInt(wbas) == 0 && Integer.parseInt(wcta) == 0) || (Integer.parseInt(wbas) > 0 && Integer.parseInt(wcta) > 0)){
+					return "Parametros Incorrectos";
+				}
+			}
 			if (Integer.parseInt(wcta) > 0) {
 				Tap002 obj = myDaoTap002.getUsingWcta(ds, wcta);
 				if (obj == null)
-					return false;
+					return "Cuenta Inexistente";
 				else
-					return true;
+					return "";
 			}
 			if (Integer.parseInt(wbas) > 0) {
 				Tap002 obj = myDaoTap002.getUsingWbas(ds, wbas);
 				if (obj == null)
-					return false;
+					return "Numero de Base Inexistente";
 				else
-					return true;
-			}
-			if ((Integer.parseInt(wbas) == 0 && Integer.parseInt(wcta) == 0) || (Integer.parseInt(wbas) > 0 && Integer.parseInt(wcta) > 0)){
-				return false;
+					return "";
 			}
 			if (Integer.parseInt(wvigd) > 0) {
 				if (wtod == "X")
-					return false;
+					return "Parametros Incorrectos";
 				else {
 					fecjud = format.parse(wvigd);
 				}
 			}
 			if (Integer.parseInt(wvigh) > 0) {
 				if (wtod == "X")
-					return false;
+					return "Parametros Incorrectos";
 				else
 				{
 					fecjuh = format.parse(wvigh);
@@ -172,36 +174,9 @@ public class Fer1020View2BzService extends RestBaseServerResource{
 			}
 		
 		} catch (ASException e) {
-			return false;
+			return e.getMessage();
 		}
-		return true;
-	}
-	
-	private ArrayList<Tap014> SubRut007(DataSet ds, String wcta, String wbas, String waca, String wpto, String wtod, Date fecjud, Date fecjuh, double wimp) throws ParseException, ASException {
-		Date dexaca;
-		tap002 = myDaoTap002.getUsingWcta(ds, wcta);
-		altnam = myDaoAltnam.getUsingWcta(ds, wcta);
-		if (tap002.getDmcbal().intValue() < 0) {
-			saldom = myDaoSaldom.getUsingWcta(ds, wcta);
-			if (saldom != null) {
-				if (saldom.getDexaca() > 0) {
-					//TODO: Resolver
-					dexaca = format.parse(saldom.getDexaca().toString());
-					wdias = Calendar.getInstance().getTime().getTime() - dexaca.getTime();
-				}
-				else
-				{
-					wdias = (long)0;
-				}
-			}
-			else
-				wdias = (long)0;
-		}
-		else
-			wdias = (long)0;
-		//
-		ArrayList<Tap014> lstTap014 = SubRut006(ds, wcta, wbas, waca, wpto, wtod, fecjud, fecjuh, wimp);
-		return lstTap014;
+		return "";
 	}
 	
 	private ArrayList<Tap014> SubRut006(DataSet ds, String wcta, String wbas, String waca, String wpto, String wtod, Date fecjud, Date fecjuh, double wimp) {
@@ -219,137 +194,35 @@ public class Fer1020View2BzService extends RestBaseServerResource{
 	
 	public class Fer1020Adapter {
 		
-		private long WCTA1;
-		private String NOMBRE;
-		private String DOMIC;
-		private String TELEF;
-		private Long WDIAS;
-		private BigDecimal DMCBAL;
-		private int DMCMCN;
+		private String WBAS;
 		
-		public Fer1020Adapter(Tap002 src1, Altnam src2, Saldom src3, Long wdias) {
-			this.WCTA1 = src1.getDmacct();
-			this.NOMBRE = src2.getNamel1();
-			this.DOMIC = src2.getAdres1();
-			this.TELEF = src2.getCtelef();
-			this.WDIAS = wdias;
-			this.DMCBAL = src1.getDmcbal();
-			this.DMCMCN = src1.getDmcmcn();
+		public Fer1020Adapter(String wbas) {
+			this.WBAS = wbas;
 		}
 
-		/**
-		 * @return the wCTA1
-		 */
-		public long getWCTA1() {
-			return WCTA1;
+		public String getWBAS() {
+			return WBAS;
 		}
 
-		/**
-		 * @param wCTA1 the wCTA1 to set
-		 */
-		public void setWCTA1(long wCTA1) {
-			WCTA1 = wCTA1;
-		}
-
-		/**
-		 * @return the nOMBRE
-		 */
-		public String getNOMBRE() {
-			return NOMBRE;
-		}
-
-		/**
-		 * @param nOMBRE the nOMBRE to set
-		 */
-		public void setNOMBRE(String nOMBRE) {
-			NOMBRE = nOMBRE;
-		}
-
-		/**
-		 * @return the dOMIC
-		 */
-		public String getDOMIC() {
-			return DOMIC;
-		}
-
-		/**
-		 * @param dOMIC the dOMIC to set
-		 */
-		public void setDOMIC(String dOMIC) {
-			DOMIC = dOMIC;
-		}
-
-		/**
-		 * @return the tELEF
-		 */
-		public String getTELEF() {
-			return TELEF;
-		}
-
-		/**
-		 * @param tELEF the tELEF to set
-		 */
-		public void setTELEF(String tELEF) {
-			TELEF = tELEF;
-		}
-
-		/**
-		 * @return the wDIAS
-		 */
-		public Long getWDIAS() {
-			return WDIAS;
-		}
-
-		/**
-		 * @param wDIAS the wDIAS to set
-		 */
-		public void setWDIAS(Long wDIAS) {
-			WDIAS = wDIAS;
-		}
-
-		/**
-		 * @return the dMCBAL
-		 */
-		public BigDecimal getDMCBAL() {
-			return DMCBAL;
-		}
-
-		/**
-		 * @param dMCBAL the dMCBAL to set
-		 */
-		public void setDMCBAL(BigDecimal dMCBAL) {
-			DMCBAL = dMCBAL;
-		}
-
-		/**
-		 * @return the dMCMCN
-		 */
-		public int getDMCMCN() {
-			return DMCMCN;
-		}
-
-		/**
-		 * @param dMCMCN the dMCMCN to set
-		 */
-		public void setDMCMCN(int dMCMCN) {
-			DMCMCN = dMCMCN;
+		public void setWBAS(String wBAS) {
+			WBAS = wBAS;
 		}
 
 	}
 
 	public class Fer1020SFLAdapter {
+		private Long WCTA1;
 		private String DESTIP;
 		private Long DOLIDO;
+		private String DMCMCN;
 		private Date WSDATC;
 		private Date WSAMND;
-		private BigDecimal DOAGGI;
 		private BigDecimal WSINTE;
 		
-		public Fer1020SFLAdapter(Tap014 src/*, Saldom src2*/) {
-
-			// TODO: Consultar si necesitamos el SALDOM en este registro
+		public Fer1020SFLAdapter(Tap014 src) {
+			this.WCTA1 = src.getDmacct();
 			try {
-				this.WSDATC = new SimpleDateFormat("yyyyMMDD").parse(src.getDodatc().toString());
+				this.WSDATC = new SimpleDateFormat("yyyyMMdd").parse(src.getDodatc().toString());
 			} catch( Exception e ) {}
 			if( src.getDmtodf().equals(1)) {
 				this.DESTIP = "ACA";
@@ -357,102 +230,70 @@ public class Fer1020View2BzService extends RestBaseServerResource{
 				this.DESTIP = "PTO";
 			} else if( src.getDmtodf().equals(9)) {
 				this.DESTIP = "TOD";
-//				try {
-//					this.WSDATC = new SimpleDateFormat("yyyyMMDD").parse(src2.getDexaca().toString());
-//				} catch( Exception e ) {}
-			}
 			this.DOLIDO = src.getDolido();
-			this.DOAGGI = src.getDoaggi();
-
 			try {
-				this.WSAMND = new SimpleDateFormat("yyyyMMDD").parse(src.getDoamnd().toString());
+				this.WSAMND = new SimpleDateFormat("yyyyMMdd").parse(src.getDoamnd().toString());
 			} catch( Exception e ) {}
 			this.WSINTE = src.getDotacc();
 		}
 
-		/**
-		 * @return the dESTIP
-		 */
+	}
+
+		public Long getWCTA1() {
+			return WCTA1;
+		}
+
+		public void setWCTA1(Long wCTA1) {
+			WCTA1 = wCTA1;
+		}
+
 		public String getDESTIP() {
 			return DESTIP;
 		}
 
-		/**
-		 * @param dESTIP the dESTIP to set
-		 */
 		public void setDESTIP(String dESTIP) {
 			DESTIP = dESTIP;
 		}
 
-		/**
-		 * @return the dOLIDO
-		 */
 		public Long getDOLIDO() {
 			return DOLIDO;
 		}
 
-		/**
-		 * @param dOLIDO the dOLIDO to set
-		 */
 		public void setDOLIDO(Long dOLIDO) {
 			DOLIDO = dOLIDO;
 		}
 
-		/**
-		 * @return the wSDATC
-		 */
+		public String getDMCMCN() {
+			return DMCMCN;
+		}
+
+		public void setDMCMCN(String dMCMCN) {
+			DMCMCN = dMCMCN;
+		}
+
 		public Date getWSDATC() {
 			return WSDATC;
 		}
 
-		/**
-		 * @param wSDATC the wSDATC to set
-		 */
 		public void setWSDATC(Date wSDATC) {
 			WSDATC = wSDATC;
 		}
 
-		/**
-		 * @return the wSAMND
-		 */
 		public Date getWSAMND() {
 			return WSAMND;
 		}
 
-		/**
-		 * @param wSAMND the wSAMND to set
-		 */
 		public void setWSAMND(Date wSAMND) {
 			WSAMND = wSAMND;
 		}
 
-		/**
-		 * @return the dOAGGI
-		 */
-		public BigDecimal getDOAGGI() {
-			return DOAGGI;
-		}
-
-		/**
-		 * @param dOAGGI the dOAGGI to set
-		 */
-		public void setDOAGGI(BigDecimal dOAGGI) {
-			DOAGGI = dOAGGI;
-		}
-
-		/**
-		 * @return the wSINTE
-		 */
 		public BigDecimal getWSINTE() {
 			return WSINTE;
 		}
 
-		/**
-		 * @param wSINTE the wSINTE to set
-		 */
 		public void setWSINTE(BigDecimal wSINTE) {
 			WSINTE = wSINTE;
 		}
-
+	
 	}
 }
